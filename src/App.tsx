eeb95ae236581b3dc34c3de4a779e80e73a227e9
Niklas Fransson <ceniklas@gmail.com>
2021-04-27 14:56:12 +0200

    Texts

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="footer">
-        <a className="message" href="marvel.html"></a>
+        <a className="message" href="marvel.html">Marvel</a>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="message">
           Hi. I'm Niklas Fransson.

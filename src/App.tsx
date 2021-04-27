352577c816115940f50cf3e28523f45ca785a1fb
Niklas Fransson <ceniklas@gmail.com>
2021-04-27 13:38:02 +0200

    marvel

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,13 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="App">
+      <div className="footer">
+        <a className="message" href="marvel.html"></a>
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <h1 className="message">
           Hi. I'm Niklas Fransson.
         </h1>
+      </div>
     </div>
   );
 }

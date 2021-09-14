ad6926968d42a76f12f9b1ce852b290f57898615
Niklas Fransson <ceniklas@gmail.com>
2021-09-14 14:46:18 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,11 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <div className="footer">
-        <a className="message" href="cush.html">cush</a>
-        <a className="message" href="executeorder66.html">executeorder66</a>
-        <a className="message" href="marvel.html">Marvel</a>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <div className="linkWrapper">
+          <a className="message" href="cush.html">cush</a>
+          <a className="message" href="executeorder66.html">executeorder66</a>
+          <a className="message" href="marvel.html">Marvel</a>
+        </div>
         <h1 className="message">
           Hi. I'm Niklas Fransson.
         </h1>

6206f77f0d086b126535276fb8ba56ff0a009ba3
Niklas 'Nisse' Fransson <niklas.fransson@getbright.se>
2021-10-01 13:23:14 +0200

    WIP lots and lots of changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ const App: React.FC = () => {
           <a className="linkmessage" href="cush.html">cush</a>
           <a className="linkmessage" href="executeorder66.html">executeorder66</a>
           <a className="linkmessage" href="marvel.html">Marvel</a>
-          <a className="linkmessage" href="norra_grantorps/index.html">Marvel</a>
+          <a className="linkmessage" href="norra_granstorps/index.html">Norra Granstorpsvägen 2</a>
         </div>
         <h1 className="message">
           Hi. I'm Niklas Fransson.

9eedb690b736947ac55d9f081598becdeeee44a3
ceniklas <ceniklas@users.noreply.github.com>
2022-09-12 15:17:13 +0000

    deploy: cd9a1f5040c32a456726603b584f4335fee82a21

--- poll.js
+++ poll.js
@@ -1,4 +1,3 @@
-"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -8,14 +7,10 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const axios_1 = __importDefault(require("axios"));
+import axios from 'axios';
 const url = 'https://valresultat.svt.se/2022/';
 const fetchData = () => __awaiter(void 0, void 0, void 0, function* () {
-    const { data } = yield axios_1.default.get(url);
+    const { data } = yield axios.get(url);
     const result = data.match(/val_result-folding-fan-labels__label.*/g);
     const res = [];
     result === null || result === void 0 ? void 0 : result.forEach(r => {

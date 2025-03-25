948e60390fc263c834b98cafd165e525a16a8433
Niklas 'Nisse' Fransson <niklas.fransson@getbright.se>
2025-03-25 16:52:41 +0100

    updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
-import './App.css';
-import Val2022 from './Val2022';
+import React from "react";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
     <div className="App">
       <div className="footer">
         <div className="linkWrapper">
-          <a className="linkmessage" href="cush.html">cush</a>
-          <a className="linkmessage" href="executeorder66.html">executeorder66</a>
-          <a className="linkmessage" href="marvel.html">Marvel</a>
-          <a className="linkmessage" href="norra_granstorps/index.html">Norra Granstorpsvägen 2</a>
+          <a className="linkmessage" href="cush.html">
+            cush
+          </a>
+          <a className="linkmessage" href="executeorder66.html">
+            executeorder66
+          </a>
+          <a className="linkmessage" href="marvel.html">
+            Marvel todo list
+          </a>
+          <a className="linkmessage" href="norra_granstorps/index.html">
+            Norra Granstorpsvägen 2
+          </a>
         </div>
-        <h1 className="message">
-          Hi. I'm Niklas Fransson.
-        </h1>
+        <h1 className="message">Hi. I'm Niklas Fransson.</h1>
       </div>
-      <Val2022></Val2022>
     </div>
   );
-}
+};
 
 export default App;
